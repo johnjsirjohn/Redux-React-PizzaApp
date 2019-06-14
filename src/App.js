cc10cc3eb8f880bc162714a3a55afa5734c0236a
johnjsirjohn <ionut.sirbu@hotmail.com>
2019-06-14 16:37:08 +0200

    React+Redux AddPizza App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+import PizzaListContainer from './components/PizzaListContainer';
+import PizzaDetailsContainer from './components/PizzaDetailsContainer';
+import AddPizzaFormContainer from './components/AddPizzaFormContainer';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <PizzaListContainer />
+        <PizzaDetailsContainer />
+        <AddPizzaFormContainer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
